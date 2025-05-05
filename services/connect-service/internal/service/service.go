879bdc7cd777b6023beee809deffc286c8a352fe
Artem09076 <grigoranartem592@yandex.ru>
2025-05-05 23:13:31 +0300

    enjoy

---
+++ services/connect-service/internal/service/service.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+	"s0709-22/internal/proxyproto"
+	"s0709-22/internal/userdb"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Service ...
+type Service struct {
+	proxyproto.UnimplementedCentrifugoProxyServer
+	conn    *pgxpool.Pool
+	storage *userdb.Queries
+}
+
+// New ...
+func New(uri string) (*Service, error) {
+	connCfg, err := pgxpool.ParseConfig(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := pgxpool.NewWithConfig(context.Background(), connCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Service{
+		conn:    conn,
+		storage: userdb.New(conn),
+	}, nil
+}
